827eb62dcc3ab099af3fb57a89278754ca221c6d
AboubakrNasef <nasefabobakr725@gmail.com>
2023-04-02 15:28:23 +0200

    Add project files.

---
+++ AutoCAD API Practice/AutoCAD_App_Services.cs
@@ -0,0 +1,39 @@
+﻿using AAAS = Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_API_Practice
+{
+    public class AutoCAD_App_Services : IExtensionApplication
+    {
+        [CommandMethod("Hello")]
+        public void Hello()
+        {
+            AAAS.Document doc = AAAS.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("Hello First Command");
+        }
+
+        #region IExtentionApplication
+
+        public void Initialize()
+        {
+            AAAS.Document doc = AAAS.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("Hello First Command");
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ AutoCAD API Practice/AutoCAD_Commands.cs
@@ -0,0 +1,42 @@
+﻿using AAAS = Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+    [assembly:CommandClass(typeof(AutoCAD_API_Practice.AutoCAD_Commands))]
+namespace AutoCAD_API_Practice
+{
+    public class AutoCAD_Commands:IExtensionApplication
+    {
+        [CommandMethod("Hello")]
+        public void Hello()
+        {
+            AAAS.Document doc = AAAS.Application.DocumentManager.MdiActiveDocument;
+            Editor ed=doc.Editor;
+            ed.WriteMessage("Hello First Command");
+        }
+
+        #region IExtentionApplication
+
+        public void Initialize()
+        {
+            AAAS.Document doc = AAAS.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("Hello First Command");
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
